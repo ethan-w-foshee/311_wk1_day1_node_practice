f483575ea8b9c83addefe54330637370360c91c1
ethan.w.foshee <skyhailer2@gmail.com>
2023-01-05 19:19:27 -0600

    Homework completed

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let next;
+  let arr = [];
+  for(let i = 0; i <= str.length - 1; i += 2) {
+    next = str.substr(i, 2)
+    arr.push(next)
+  }
+  return arr
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split("").reverse().join("")
 }
 
 module.exports = {
